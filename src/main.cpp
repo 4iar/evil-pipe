9b7baf30ec198172fd886a075b5ca81c1bd60d7b
4iar <me@4iar.com>
2017-06-26 19:54:27 +0100

    Add main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdexcept>
+
+#include "server.hpp"
+
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        throw std::invalid_argument("Invalid arguments. Correct format: ./evil-pipe <port>");
+    }
+
+    const short port = atoi(argv[1]);
+    std::cout << "Server running @ port " << port << std::endl;
+
+    Server server(port);
+    server.start();
+}
